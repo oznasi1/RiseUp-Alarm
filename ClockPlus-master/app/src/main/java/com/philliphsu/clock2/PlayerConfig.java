b4094a7320762787cadb17883c4219992c58beeb
oznasi1 <oznasi1@gmail.com>
2018-08-31 17:40:47 +0300

    Update PlayerConfig.java

--- ClockPlus-master/app/src/main/java/com/philliphsu/clock2/PlayerConfig.java
+++ ClockPlus-master/app/src/main/java/com/philliphsu/clock2/PlayerConfig.java
@@ -1,7 +1,7 @@
 package com.philliphsu.clock2;
 
 public class PlayerConfig {
-    public static final String API_KEY ="AIzaSyCtOmsroTpZ2fNMZ9rZ0PVC7z4JlvQkw6c";
+    public static final String API_KEY ="";
 
     PlayerConfig(){
 
